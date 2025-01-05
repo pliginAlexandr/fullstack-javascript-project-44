c5a391ebce7568a62bf4875ac3b2e2ba15ceceb9
Alexandr Pligin <rogchik@gmail.com>
2025-01-05 11:08:48 +0500

    eslint fix

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greetings from '../src/cli.js';
 import { getRandomInt, isEven, gamesLogic } from '../src/index.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-
 const brainEvenGenerateRound = () => {
-  const question = getRandomInt(100); //значение в скобках устанавливает максимально возможное генерируемое число.
+  const question = getRandomInt(100); // значение устанавливает максимальное генерируемое число.
   const correctAnswer = isEven(question);
   return [question, correctAnswer];
 };
