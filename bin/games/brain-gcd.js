466c7caca39348031b61ffd5e1d21196470a8083
Alexandr Pligin <rogchik@gmail.com>
2025-01-06 09:49:33 +0500

    brain-gcd added

---
+++ bin/games/brain-gcd.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+import { getRandomInt, gamesLogic } from '../../src/index.js';
+
+const rules = 'Find the greatest common divisor of given numbers.';
+
+const brainGcdGenerateRound = () => {
+  let num1 = getRandomInt(50);
+  let num2 = getRandomInt(50);
+
+  const question = `${num1} ${num2}`;
+
+
+  while (num1 !== 0 && num2 !== 0) {
+    if (num1 > num2) {
+      num1 %= num2;
+    } else {
+      num2 %= num1;
+    }
+  }
+  const correctAnswer = String(num1 + num2);
+
+  return [question, correctAnswer];
+};
+
+gamesLogic(brainGcdGenerateRound, rules);
